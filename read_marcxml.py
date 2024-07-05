4f55cdb0af219680a659717a5ed3e1a8f0650ee8
humayrah <124104680+humayrah05@users.noreply.github.com>
2024-07-05 13:52:52 -0400

    Create read_marcxml.py
    Reads and prints required fields from a marcxml file

---
+++ read_marcxml.py
@@ -0,0 +1,45 @@
+from pymarc import parse_xml_to_array
+
+# reads and prints specific fields of a marcxml file
+
+file = 
+
+# checks if record is empty - if not, prints record fields
+with open(file, 'rb') as fh:
+    reader = parse_xml_to_array(fh)
+    for record in reader:
+        if record is not None:
+            print("Full record")
+        else:
+            print("Empty record encountered")
+    
+for record in reader:
+    #title
+    print ('title: ' + record['245']['a'])
+
+    #creator
+    print ('creator: ' + record['100']['a'])
+
+    #additional creators
+    for f in record.get_fields('700'):
+        subfield_a = f.get_subfields('a')
+        for value in subfield_a:
+            print('additional creators: ' + value)
+
+    #publisher
+    print ('publisher: ' + record['260']['a'] + record['260']['b'])
+
+    #date
+    print ('date: ' + record['260']['c'])
+
+    #description
+    for f in record.get_fields('505'):
+        subfield_a = f.get_subfields('a')
+        for value in subfield_a:
+            print('description: ' + value)
+
+    #subject
+    for f in record.get_fields('650'):
+        subfield_a = f.get_subfields('a')
+        for value in subfield_a:
+            print('subject: ' + value)
