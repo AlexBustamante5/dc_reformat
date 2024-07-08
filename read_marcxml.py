87ae0e2f5fe5c70803315911962aa08e9c272a53
humayrah <124104680+humayrah05@users.noreply.github.com>
2024-07-08 10:42:19 -0400

    Added error handling

--- read_marcxml.py
+++ read_marcxml.py
@@ -1,45 +1,59 @@
 from pymarc import parse_xml_to_array
 
-# reads and prints specific fields of a marcxml file
+# reads a marcxml file
 
-file = 
+def main(file):
+    try:
+        # checks if record is empty - if not, prints record fields
+        with open(file, 'rb') as fh:
+            reader = parse_xml_to_array(fh)
+            for record in reader:
+                if record is not None:
+                    print("Full record")
+                    read(record)
+                else:
+                    print("Empty record encountered")
+    except FileNotFoundError:
+        print(f"Error: {file} not found.")
+    except Exception as e:
+        print(f"Error: {e}")
 
-# checks if record is empty - if not, prints record fields
-with open(file, 'rb') as fh:
-    reader = parse_xml_to_array(fh)
-    for record in reader:
-        if record is not None:
-            print("Full record")
-        else:
-            print("Empty record encountered")
-    
-for record in reader:
+def read(record):
+
     #title
-    print ('title: ' + record['245']['a'])
+    title_a = record['245']['a']
+    title_b = record['245']['b'] if record['245'] and 'b' in record['245'] else ' '
+    title = title_a + title_b
+    print(f'title: {title}')
 
     #creator
-    print ('creator: ' + record['100']['a'])
+    creator = record['100']['a']
+    print(f'creator: {creator}')
 
     #additional creators
-    for f in record.get_fields('700'):
-        subfield_a = f.get_subfields('a')
-        for value in subfield_a:
-            print('additional creators: ' + value)
+    for field in record.get_fields('700'):
+        for subfield_a in field.get_subfields('a'):
+            print(f'additional creators: {subfield_a}')
 
     #publisher
-    print ('publisher: ' + record['260']['a'] + record['260']['b'])
+    publisher = record['260']['a'] + record['260']['b']
+    print(f'publisher: {publisher}')
 
     #date
-    print ('date: ' + record['260']['c'])
+    date = record['260']['c']
+    print(f'date: {date}')
 
     #description
     for f in record.get_fields('505'):
         subfield_a = f.get_subfields('a')
         for value in subfield_a:
-            print('description: ' + value)
+            print(f'description: {value}')
 
     #subject
     for f in record.get_fields('650'):
         subfield_a = f.get_subfields('a')
         for value in subfield_a:
-            print('subject: ' + value)
+            print(f'subject: {value}')
+#file path
+file = 
+main(file)
